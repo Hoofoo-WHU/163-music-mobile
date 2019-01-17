9ce9659acc3db339d799905d5105668ffb0d69f4
Hoofoo <wangshuophone@icloud.com>
2019-01-17 11:28:52 +0800

    基本的播放控制

--- src/play/js/play.js
+++ src/play/js/play.js
@@ -4,6 +4,22 @@
   let urlParams = new URLParams(window.location)
   ~async function () {
     let song = await http.getMusic(urlParams.id)
-    $('body').append($(`<audio src="//${song.url}" controls autoplay></audio>`))
+    console.log(song)
+    let audio = $(`<audio src="//${song.url}" controls autoplay></audio>`)
+    $('body').append(audio)
+    function play() {
+      if ($(this).html() === '播放') {
+        $(this).html('暂停')
+        audio.trigger('play')
+      } else {
+        $(this).html('播放')
+        audio.trigger('pause')
+      }
+    }
+    $('body').append($(`<img src="//${song.cover}" style="width:100vw">`))
+    $('body').append($(`<h2>歌名：${song.name}</h2>`))
+    $('body').append($(`<h2>歌手：${song.singer}</h2>`))
+    $('body').append($(`<h2>专辑：${song.album}</h2>`))
+    $('body').append($(`<button>播放</button>`).on('click', play))
   }()
 }
