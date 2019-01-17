0305c6e9f48333d603b2f714d09ef7bb6b76f70e
Hoofoo <wangshuophone@icloud.com>
2019-01-17 16:42:40 +0800

    封面背景

--- src/play/js/play.js
+++ src/play/js/play.js
@@ -6,7 +6,7 @@
     let song = await http.getMusic(urlParams.id)
     console.log(song)
     let audio = $(`<audio src="//${song.url}" controls autoplay></audio>`)
-    $('body').append(audio)
+    $('#app>main').append(audio)
     function play() {
       if ($(this).html() === '播放') {
         $(this).html('暂停')
@@ -16,10 +16,10 @@
         audio.trigger('pause')
       }
     }
-    $('body').append($(`<img src="//${song.cover}" style="width:100vw">`))
-    $('body').append($(`<h2>歌名：${song.name}</h2>`))
-    $('body').append($(`<h2>歌手：${song.singer}</h2>`))
-    $('body').append($(`<h2>专辑：${song.album}</h2>`))
-    $('body').append($(`<button>播放</button>`).on('click', play))
+    $('#back').css({ 'background-image': `url(//${song.cover})` })
+    $('#app>main').append($(`<h2>歌名：${song.name}</h2>`))
+    $('#app>main').append($(`<h2>歌手：${song.singer}</h2>`))
+    $('#app>main').append($(`<h2>专辑：${song.album}</h2>`))
+    $('#app>main').append($(`<button>播放</button>`).on('click', play))
   }()
 }
